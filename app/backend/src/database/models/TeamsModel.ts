79dd1b419be4856262c3ff4e2493bb0dd627ef74
Victor Cabral <victor_cabral_o@hotmail.com>
2023-03-28 19:15:02 -0300

    add: start

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,26 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Team.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  teamName: {
+    type: STRING(255),
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Team;
